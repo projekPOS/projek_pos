bd93c6263fa74404c3dd2f77c698b84a503d81ad
Haris Adiyatma Farhan <adiyatmaharis8@gmail.com>
2023-03-03 20:54:12 +0700

    Login_Base

---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>POS NI ?</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-6">
+        <section class="left-main">
+
+        </section>
+        </div>
+        <div class="col-md-6">
+        <section class="right-main">
+
+        </section>
+        </div>
+</div>
+    
+</body>
+<script src="{{asset('js/app.js')}}"></script>
+</html>
